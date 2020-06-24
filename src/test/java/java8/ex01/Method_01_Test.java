e34810ff4a126b613a08c2ac35c39d1783a5d77b
Alexis PRODHOMME <alexis.prodhom@hotmail.fr>
2020-06-24 11:30:57 +0200

    TP default method

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -19,9 +19,18 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        default int sumAge() {
+        	int sum = 0;
+        	for ( Person person : findAll()) {
+        		sum += person.getAge();
+        	}
+        	return sum;
+        }
     }
     // end::IDao[]
 
+    
+    
     class DaoA implements IDao {
 
         List<Person> people = Data.buildPersonList(20);
@@ -42,24 +51,28 @@ public class Method_01_Test {
         }
     }
 
+    
+    
     @Test
     public void test_daoA_sumAge() throws Exception {
 
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();;
 
         assert result == 210;
     }
 
+    
+    
     @Test
     public void test_daoB_sumAge() throws Exception {
 
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
