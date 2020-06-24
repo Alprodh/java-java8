1dec154f7cda5cb92756fb0d028499da2f367dce
Alexis PRODHOMME <alexis.prodhom@hotmail.fr>
2020-06-24 17:31:02 +0200

    TP fonctions

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -16,9 +16,9 @@ public class Function_06_Test {
     // tag::formatAge[]
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
-    String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+    String formatAge( Supplier<Person> supplier) {
+    	String str = "[age=" + supplier.get().getAge() + "]"; 
+        return str;
     }
     // end::formatAge[]
 
@@ -26,7 +26,8 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+    	Supplier<Person> supplierJohn = () -> new Person("John", "France", 35, "johndoe");
+        String result = formatAge( supplierJohn);
 
         assert result.equals("[age=35]");
     }
